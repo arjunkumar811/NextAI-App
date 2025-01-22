73775875cb4daaa4407e5ef4f8a66503fd7d72e3
arjunkumar811 <kumararjun26401@gmail.com>
2025-01-22 22:00:36 +0530

    otp

--- src/helpers/sendVatificationemail.ts
+++ src/helpers/sendVatificationemail.ts
@@ -9,9 +9,9 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'dev@hiteshchoudhary.com',
+      from: 'dev@kumararjun26401@gmail.com',
       to: email,
-      subject: 'Mystery Message Verification Code',
+      subject: ' Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
     return { success: true, message: 'Verification email sent successfully.' };
